217353bbae746c8e1457c66d1fd15eab9093e4f7
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-08-28 14:14:00 +0100

    test: disburse

--- tests/DisburseTest.php
+++ tests/DisburseTest.php
@@ -20,6 +20,82 @@ use BrokeYourBike\PolarisBank\Client;
 class DisburseTest extends TestCase
 {
     /** @test */
+    public function it_can_prepare_request(): void
+    {
+        $transaction = $this->getMockBuilder(TransactionInterface::class)->getMock();
+
+        /** @var TransactionInterface $transaction */
+        $this->assertInstanceOf(TransactionInterface::class, $transaction);
+
+        $mockedConfig = $this->getMockBuilder(ConfigInterface::class)->getMock();
+        $mockedConfig->method('getUrl')->willReturn('https://api.example/');
+        $mockedConfig->method('getToken')->willReturn('john');
+        $mockedConfig->method('getSecret')->willReturn('password');
+        $mockedConfig->method('isMock')->willReturn(false);
+
+        $mockedResponse = $this->getMockBuilder(ResponseInterface::class)->getMock();
+        $mockedResponse->method('getStatusCode')->willReturn(200);
+        $mockedResponse->method('getBody')
+            ->willReturn('{
+                "status": "Successful",
+                "message": "Disburse successful",
+                "data": {
+                    "provider_response_code": "00",
+                    "provider": "Polaris",
+                    "errors": null,
+                    "error": null,
+                    "provider_response": {
+                        "reference": "0012345",
+                        "payment_id": "123412l3j4h1234",
+                        "destination_institution_code": "000016",
+                        "beneficiary_account_name": "JOHN DOE",
+                        "beneficiary_account_number": "81273412394",
+                        "beneficiary_kyc_level": "3",
+                        "originator_account_name": "",
+                        "originator_account_number": "123419234",
+                        "originator_kyc_level": "1",
+                        "narration": "USSD NIP Transfer",
+                        "transaction_final_amount": 1234215
+                    },
+                    "client_info": {
+                        "name": null,
+                        "id": null,
+                        "bank_cbn_code": null,
+                        "bank_name": null,
+                        "console_url": null,
+                        "js_background_image": null,
+                        "css_url": null,
+                        "logo_url": null,
+                        "footer_text": null,
+                        "show_options_icon": false,
+                        "paginate": false,
+                        "paginate_count": 0,
+                        "options": null,
+                        "merchant": null,
+                        "colors": null,
+                        "meta": null
+                    }
+                }
+            }');
+
+        /** @var \Mockery\MockInterface $mockedClient */
+        $mockedClient = \Mockery::mock(\GuzzleHttp\Client::class);
+        $mockedClient->shouldReceive('request')->once()->andReturn($mockedResponse);
+
+        /**
+         * @var ConfigInterface $mockedConfig
+         * @var \GuzzleHttp\Client $mockedClient
+         * */
+        $api = new Client($mockedConfig, $mockedClient);
+
+        $requestResult = $api->disburse('req-123', $transaction);
+        $this->assertInstanceOf(TransactionResponse::class, $requestResult);
+        $this->assertEquals('Successful', $requestResult->status);
+        $this->assertEquals('Disburse successful', $requestResult->message);
+        $this->assertEquals('0012345', $requestResult->providerReference);
+    }
+
+    /** @test */
     public function it_can_handle_failed_response(): void
     {
         $transaction = $this->getMockBuilder(TransactionInterface::class)->getMock();
@@ -88,5 +164,9 @@ class DisburseTest extends TestCase
 
         $requestResult = $api->disburse('req-123', $transaction);
         $this->assertInstanceOf(TransactionResponse::class, $requestResult);
+        $this->assertEquals('Failed', $requestResult->status);
+        $this->assertEquals('Unable to process transaction', $requestResult->message);
+        $this->assertEquals('04', $requestResult->errorCode);
+        $this->assertEquals('Unable to processs transaction', $requestResult->errorMessage);
     }
 }
